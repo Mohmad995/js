64ef5a4bf9574a73b70ade5d9ce6626f8ff69ed2
Magomed Dulaev <maga.aga.1996@mail.ru>
2022-08-16 19:05:37 +0300

    js

---
+++ index.js
@@ -0,0 +1,85 @@
+// // console.log("Hello. World!")
+// // console.log(2+3)
+// // console.log(10-2);
+// // console.log(2*3);
+// // console.log(10/5);
+// // console.log(2**3);
+// // console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3))
+
+// // let, const
+// //  let number =2
+// // let n1 = 2+2
+// // let n2 = 2*4 
+// // let n3 = 10/2
+// // let n4 = 4-1 
+// // let n5 = 2**3
+// // console.log(n1 + n2 + n3 + n4 + n5);
+// // n1 = 10+2
+// // console.log(n1 + n2 + n3 + n4 + n5);
+// // console.log(number); 
+
+// // let age = 32
+// // console.log(age); 
+// // console.log("спустя год age будет", age + 1);
+
+// // Типы данных в JS 
+// //  let num = 1 number 
+
+// // let str1 = "My name is Mohmad" //string 
+// // let str2 = 'My name is Mohmad'
+// // let str3 = `My name is Mohmad`
+// // console.log(str3);
+
+// // let bool1 = true // boolean
+// // let bool2 = false 
+
+// // // undefined, null, object, symbol 
+
+// // str3 = "My name is Cool"
+// // console.log(str3);
+
+ 
+// // let n1 = 2+6
+// // let n2 = 10-6 
+// // let n3 = 12/6
+// // let n4 = 4*9 
+// // let n5 = 3**6
+// // console.log (n1,n2,n3,n4,n5);
+// // n1 = 2+10
+// // console.log (n1,n2,n3,n4,n5); 
+// // n3 = 12/8
+// // console.log (n1,n2,n3,n4,n5);
+// // n5 = 3**9
+// // console.log (n1,n2,n3,n4,n5);
+
+
+
+// // let str1 = "Хочу домой"
+// // let str2 = "Думаю все нормально"
+// // let str3 = "Как Дела"
+// // let str4 = "Все плохо"
+// // let str5 = "Я все это конец"
+//             0 1 2 3 4 5 6
+// // let nums = [1,2,3,4,5,6,7]
+// // console.log(nums);
+// // let names = ['Adam', 'Ali']
+// // console.log(names);
+// // console.log(names)[0];
+
+// let nameAge = ["Mohmad", 26] 
+// console.log("Имя:", nameAge[0], ",", "Возраст:", nameAge[1]);
+
+// let nameAge1 = ["Ibra", 25]
+// console.log("Имя:", nameAge1[0], ",", "Возраст:", nameAge1[1]);
+
+// let nameAge2 = ["Nurik", 24]
+// console.log("Имя:", nameAge2[0], ",", "Возраст:", nameAge2[1]);
+
+// let nameAge3 = ["Deni", 21]
+// console.log("Имя:", nameAge3[0], ",", "Возраст:", nameAge3[1]);
+// let nameAge4 = ["Ramzan", 31]
+// console.log("Имя:", nameAge4[0], ",", "Возраст", nameAge4[1]);
+let nameAge5 = ["Maga", 21, "Ibra", 24, "Hasan", 24]
+console.log("Имя:", nameAge5[0], ",", "Возраст:", nameAge5[1]); 
+console.log("Имя:", nameAge5[2], ",", "Возраст:", nameAge5[3]); 
+console.log("Имя:", nameAge5[4], ",", "Возраст:", nameAge5[5]); 
