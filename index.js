a7acc634056f22a36aaf1e713165e9bb6537e752
Magomed Dulaev <maga.aga.1996@mail.ru>
2022-08-20 12:04:15 +0300

    круто

--- index.js
+++ index.js
@@ -38,7 +38,7 @@
 // // str3 = "My name is Cool"
 // // console.log(str3);
 
- 
+
 // // let n1 = 2+6
 // // let n2 = 10-6 
 // // let n3 = 12/6
@@ -79,7 +79,70 @@
 // console.log("Имя:", nameAge3[0], ",", "Возраст:", nameAge3[1]);
 // let nameAge4 = ["Ramzan", 31]
 // console.log("Имя:", nameAge4[0], ",", "Возраст", nameAge4[1]);
-let nameAge5 = ["Maga", 21, "Ibra", 24, "Hasan", 24]
-console.log("Имя:", nameAge5[0], ",", "Возраст:", nameAge5[1]); 
-console.log("Имя:", nameAge5[2], ",", "Возраст:", nameAge5[3]); 
-console.log("Имя:", nameAge5[4], ",", "Возраст:", nameAge5[5]); 
+// let nameAge5 = ["Maga", 21, "Ibra", 24, "Hasan", 24]
+// console.log("Имя:", nameAge5[0], ",", "Возраст:", nameAge5[1]); 
+// console.log("Имя:", nameAge5[2], ",", "Возраст:", nameAge5[3]); 
+// console.log("Имя:", nameAge5[4], ",", "Возраст:", nameAge5[5]); 
+
+// let names = "Mohmad"
+// let age = "27"
+// let names2 = "Sahab"
+// let car = "KIA"
+// console.log("My name is", names + ".", "I am", age, "My frends name", names2 + ".", "My frends car", car);
+
+// let names = [
+//     ["Movsar", 32],
+//     ["Milana", 39],
+//     ["Heda", 28],
+//     ["Ibra", 25],
+//     ["Mohmad", 27]
+// ]
+// console.log("My name is " + names[0][0] + ". i am " + names[0][1]);
+// console.log("My name is " + names[1][0] + ". i am " + names[1][1]);
+// console.log("My sister name is", names[2][0] + ". She is", names[2][1]);
+// console.log("My anme is", names[3][0] + ". i am", names[3][1]);
+// console.log("My name is", names [4][0] + ". i am", names [4][1] );
+
+// let str = "I live in Grozny"
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+// console.log(str[3]);
+// console.log(str[4]);
+// console.log(str[5]);
+// console.log(str[6]);
+// console.log(str[7]);
+// console.log(str[8]);
+// console.log(str[9]);
+// console.log(str[10]);
+// console.log(str[11]);
+// console.log(str[12]);
+// console.log(str[13]);
+// console.log(str[14]);
+// console.log(str[15]);
+
+// let index = 0 
+
+// let religion = "ISLAM"
+
+// console.log(religion[index]);
+// console.log(religion[index +1]);
+// console.log(religion[index +2] );
+// console.log(religion[index +3] );
+
+
+// let str = "ДЛИНА vvvld llksk"
+// console.log("Длина строки str= " + str.length);
+
+// let arr = [1, 2, 3, 4, 5,]
+// console.log(arr.length); 
+
+// console.log(3<5);
+// console.log(3>5);
+// console.log(3<=3);
+// console.log(3>=2);
+// console.log("ab"==="ab");
+// console.log("ad"!=="ab");
+// console.log("ad"==="ad");
+
+
